c0c9f4db5888c095f8f81634e5af3868f88d70e3
Ryang Sohn <loop.infinitely@gmail.com>
2019-10-26 16:11:55 +0900

    Fix font size glitch in Windows
    Windows sucks.

--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+from os import name
 import tkinter as tk
 import controller
 
@@ -9,7 +10,10 @@ class View:
             master.grid_columnconfigure(x, minsize=150)
             for y in range(3):
                 master.grid_rowconfigure(y, minsize=150)
-                l = tk.Label(master, text='_', font=('Helvetica', 120),
+                font_size = 120
+                if name == 'nt':
+                    font_size = 90
+                l = tk.Label(master, text='_', font=('Helvetica', font_size),
                              padx=5, pady=5)
                 l.grid(column=x, row=y)
                 line.append(l)
